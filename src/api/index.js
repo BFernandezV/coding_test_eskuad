864a7449abdc7ebc184697da58c748fda95d749a
Benjamin <bfernandez@inf.udec.cl>
2022-09-05 01:21:54 -0300

    no message

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,2 @@
-import axios from "axios";
-export default {
-  getDataArticles() {
-    axios
-      .get("https://hn.algolia.com/api/v1/search_by_date?query=coding")
-      .then((response) => {
-        this.info = response.data.bpi;
-      })
-      .catch((error) => {
-        console.log(error);
-        this.errored = true;
-      })
-      .finally(() => (this.loading = false));
-  },
-};
+/* Para un proyecto más grande es preferible separar en un archivo como este las llamadas a API */
+export default {};
